4d10db59005f2a105fb92d920340566e19258b3a
ChroNeo <chroneo47.dev@gmail.com>
2026-02-14 15:23:30 +0700

    chore : unuse file

--- apps/frontend/src/pages/Ticket/hooks/index.ts
+++ apps/frontend/src/pages/Ticket/hooks/index.ts
@@ -1,4 +1,2 @@
-export { useTickets } from './useTickets';
-export type { TicketData } from './useTickets';
-export { useWalletData } from './useWalletData';
-export type { Ticket, UserProfile } from './useWalletData';
+export { useTickets } from "./useTickets";
+export type { TicketData } from "./useTickets";
